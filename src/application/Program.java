5a7f8a9bd88cdb2a4ebd395b5d044f8c57687f62
Diego Muller <diegomaglia96@gmail.com>
2025-03-24 18:20:43 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,6 +33,16 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		
+		System.out.println();
+		System.out.println("=== Test 4: Seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg Grow", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted new seller! new id: " + newSeller.getId());
+		
+		
+		
 
 	}
 
